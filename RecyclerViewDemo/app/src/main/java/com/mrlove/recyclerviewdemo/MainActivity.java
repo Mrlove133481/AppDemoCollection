6dd4f0fe16874d90d63c515fa4855d30f599ec55
Mrlove133481 <1334819077@qq.com>
2020-05-05 17:25:01 +0800

    <create><swipeRefreshLayout><swipeRefreshLayout的简单使用>

--- RecyclerViewDemo/app/src/main/java/com/mrlove/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/mrlove/recyclerviewdemo/MainActivity.java
@@ -3,16 +3,23 @@ package com.mrlove.recyclerviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private MyAdapter adapter;
+    private ArrayList<String> arrayList;
+    private ArrayList<String> arrayList1;
+    private ArrayList<String> arrayList2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +31,40 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList = new ArrayList<>();
         for(int i=1;i<20;i++){
             arrayList.add("安卓测试"+i);
         }
+        arrayList1 = new ArrayList<>();
+        for(int i=1;i<20;i++){
+            arrayList1.add("页面刷新"+i);
+        }
 
-
-
-        MyAdapter adapter = new MyAdapter(arrayList);
+        arrayList2 = new ArrayList<>();
+        for(int i=1;i<20;i++){
+            arrayList2.add("改变数据"+i);
+        }
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                changeData();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        adapter = new MyAdapter(arrayList);
         recyclerView.setAdapter(adapter);
+    }
 
+    public void changeData(){
+        Random random = new Random();
+        ArrayList<String> arrayList3 = new ArrayList<>();
+        Boolean b = random.nextBoolean();
+        System.out.println(b);
+        arrayList3.clear();
+        arrayList3.addAll(b?arrayList1:arrayList2);
+        arrayList.clear();
+        arrayList.addAll(arrayList3);
+        adapter.notifyDataSetChanged();
     }
 }
